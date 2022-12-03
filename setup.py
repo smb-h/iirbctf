d9e62711dcf996cd565af9c87c7c61099a887262
SMB H <smbhop@pm.me>
2022-12-03 11:22:52 +0330

    :art: update(structure): reformat files

--- setup.py
+++ setup.py
@@ -1,10 +1,10 @@
 from setuptools import find_packages, setup
 
 setup(
-    name='gscmr4cqbir',
+    name="gscmr4cqbir",
     packages=find_packages(),
-    version='0.1.0',
-    description='Geometry Sensitive Cross-Modal Reasoning for Composed Query Based Image Retrieval',
-    author='SMBH',
-    license='MIT',
+    version="0.1.0",
+    description="Geometry Sensitive Cross-Modal Reasoning for Composed Query Based Image Retrieval",
+    author="SMBH",
+    license="MIT",
 )
